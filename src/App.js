1cde86bfc13cec2944c0f07020cffae016259748
sadafkhan0107 <66263424+sadafkhan0107@users.noreply.github.com>
2023-06-02 17:57:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+export const data = [
+  {
+    id: 1,
+    item: "shoes",
+		price: 100,
+		quantity: 1
+  },
+{
+    id: 2,
+    item: "jacket",
+		price: 400,
+		quantity: 1
+  },
+{
+    id: 3,
+    item: "hat",
+		price: 50,
+		quantity: 1
+  },
+{
+    id: 4,
+    item: "sunglasses",
+		price: 250,
+		quantity: 1
+  },
+{
+    id: 5,
+    item: "gloves",
+		price: 300,
+		quantity: 1
+  },
+
+];
 
 function App() {
+  const [database, setDatabase] = useState(data);
+  const [total, setTotal] = useState(0);
+  const handleIncrement = (id) =>{
+     const updatedDatabase = database.map((item) => item.id === id ? ({...item, quantity:item.quantity+1}) : item)
+    setDatabase(updatedDatabase)
+  }
+
+  const handleDecrement = (id) =>{
+    const updatedDatabase = database.map((item) => item.id === id ? ({...item,quantity: item.quantity-1}) : item)
+    setDatabase(updatedDatabase)
+  }
+// setTotal(database.reduce((acc,curr) => acc + curr.price * curr.quantity, 0));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {database.map((item ) => 
+      <div key={item.id}>
+        {item.item} ({item.price}$)
+        <button onClick={() => handleIncrement(item.id)}> + </button>
+        {item.quantity}
+        <button onClick={() => handleDecrement(item.id)}> - </button>
+      </div> 
+      )}
+      <div>
+        Total - {total}$
+      </div>
     </div>
   );
 }
